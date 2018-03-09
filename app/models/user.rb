8382f5f950cf3c974e8b997880b0dcf0b6b5a26f
Tony Cioara <tonyangelo9707@gmail.com>
2018-03-09 15:24:54 -0800

    why is my github broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
         end
     end
 
-    def encrypt_password do
+    def encrypt_password
         if password.present?
             self.password_salt = BCrypt::Engine.generate_salt
             self.password_hash = BCrypt::Engine.hash_secret(password, generate_salt)
